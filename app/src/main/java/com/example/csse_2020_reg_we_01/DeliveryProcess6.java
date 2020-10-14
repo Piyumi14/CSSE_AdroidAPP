88d043af2555e2b6e38b5ab00077aadeba4ad0bf
Piyumi14 <lbpmdayarathna@gmail.com>
2020-10-14 21:01:56 +0530

    Add interfaces For DeliveryProcess6

---
+++ app/src/main/java/com/example/csse_2020_reg_we_01/DeliveryProcess6.java
@@ -0,0 +1,14 @@
+package com.example.csse_2020_reg_we_01;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DeliveryProcess6 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_delivery_process6);
+    }
+}
