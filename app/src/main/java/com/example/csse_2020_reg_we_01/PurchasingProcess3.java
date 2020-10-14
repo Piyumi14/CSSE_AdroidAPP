ec8729a3396317d172719b1e55b13f2631c04b8e
Piyumi14 <lbpmdayarathna@gmail.com>
2020-10-14 22:30:07 +0530

    Add interfaces For DeliveryProcess modificatons

---
+++ app/src/main/java/com/example/csse_2020_reg_we_01/PurchasingProcess3.java
@@ -0,0 +1,14 @@
+package com.example.csse_2020_reg_we_01;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PurchasingProcess3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_purchasing_process3);
+    }
+}
